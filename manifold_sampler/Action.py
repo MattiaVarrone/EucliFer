free fermions implemented: update of signs for gauge link to be fixed

manifold_sampler/Action.py
@@ -62,7 +62,7 @@ def S_psi_inter(adj, psi, c, A):
         d_psi_y += d_psi * np.sin(theta)
 
     D_psi = np.matmul(id + gamma1, d_psi_x) / 2 + np.matmul(id + gamma2, d_psi_y) / 2
-    psi_bar = np.conj(np.matmul(gamma1, psi[c]))  ### check how to calc psi_bar
+    psi_bar = np.conj(np.matmul(psi[c], eps))  ### check how to calc psi_bar
     S = - K * np.imag(np.matmul(psi_bar, D_psi)) + _mass * np.real(np.matmul(psi_bar, psi[c]))
     return S
 
@@ -74,16 +74,15 @@ def S_psi_free(adj, psi, c, sign):
         j = 3 * c + i
         adj_c = adj[j] // 3
         alpha = theta[i] - theta[adj[j] % 3] + np.pi
-
-        U = sign[j]*paral_trans(alpha/2)                # factor of 1/2 accounts for spinor transport (I should store the possible values of U)
-
+        U = sign[j] * paral_trans(alpha/2)            # factor of 1/2 accounts for spinor transport
+                                                      # (I should store the possible values of U)
         d_psi = (np.matmul(U, psi[adj_c]) - psi[c])
         d_psi_x += d_psi * np.cos(theta[i])
         d_psi_y += d_psi * np.sin(theta[i])
 
     D_psi = np.matmul(id + gamma1, d_psi_x) / 2 + np.matmul(id + gamma2, d_psi_y) / 2
-    psi_bar = np.conj(np.matmul(gamma1, psi[c]))      ### check how to calc psi_bar
-    S = - K * np.imag(np.matmul(psi_bar, D_psi)) + _mass * np.real(np.matmul(psi_bar, psi[c]))
+    psi_bar = np.matmul(eps, psi[c])      ### check how to calc psi_bar
+    S = - K * np.matmul(psi_bar, D_psi) + _mass * np.matmul(psi_bar, psi[c])
     return S
 
 ###check action calculation thoroughly     ### check why psi tends to diverge

manifold_sampler/Triangulation.py
@@ -4,6 +4,24 @@ from Graph_utils import *
 rng = np.random.default_rng()
 
 
+def circle_vertex(adj, sign, i):
+    j = next_(adj[i])
+    alpha = theta[i % 3] - theta[adj[i] % 3] + np.pi
+    U = sign[i] * paral_trans(alpha / 2)
+    def_angle = 2 * np.pi - np.pi / 3
+
+    # clockwise circling of vertices
+    while j != i:
+        alpha = theta[j % 3] - theta[adj[j] % 3] + np.pi
+        U_1 = sign[j] * paral_trans(alpha / 2)
+        U = np.matmul(U, U_1)
+        def_angle -= np.pi / 3
+        j = next_(adj[j])
+
+    trace = np.trace(U) / 2
+    return trace, def_angle
+
+
 class Manifold:
     # Simplicial Manifold: created by piecing triangles together with the topology of a sphere
 
@@ -14,9 +32,9 @@ class Manifold:
         self.phi = np.zeros(N)
         self.sigma = np.ones(N)
 
-        self.psi = np.zeros((N, 2), dtype=np.complex_)
+        self.psi = np.zeros((N, 2), dtype=np.complex_)  ### should use majorana basis eventually
         self.A = np.zeros(3 * N)  # variable gauge link
-        self.sign = np.ones(3 * N)  # gauge link sign
+        self.sign = fan_sign(self.adj)  # gauge link sign
 
     def random_update(self, beta, strategy):
         if 'gravity' in strategy:
@@ -31,7 +49,7 @@ class Manifold:
         if 'spinor_free' in strategy:
             random_centre = rng.integers(0, self.N)
             self.psi = self.update_field(random_centre, self.psi, psi_range,
-                                         action=S_psi_free, beta=beta, is_complex=True, add_field=self.sign)
+                                         action=S_psi_free, beta=beta, is_complex=False, add_field=self.sign)
         if 'spinor_inter' in strategy:
             random_centre, random_side = rng.integers(0, self.N), rng.integers(0, len(self.adj))
             self.psi = self.update_field(random_centre, self.psi, psi_range,
@@ -79,8 +97,11 @@ class Manifold:
                 S_new = S_sigma(adj_new, self.sigma, c1) + S_sigma(adj_new, self.sigma, c2)
                 dS += S_new - S_old
             if 'spinor_free' in strategy:
-                # signs of parallel transporters need to be changed to ensure positive plaquette sign. (A drawing would be useful)
-
+                # signs of parallel transporters need to be changed to ensure positive plaquette sign.
+                for edge in (n, m, l):
+                    trace, def_angle = circle_vertex(adj_new, sign_new, edge)
+                    sign_new[edge] = np.sign(trace / np.cos(def_angle/2))
+                    sign_new[adj_new[edge]] = sign_new[edge]
 
                 S_old = S_psi_free(self.adj, self.psi, c1, self.sign) + S_psi_free(self.adj, self.psi, c2, self.sign)
                 S_new = S_psi_free(adj_new, self.psi, c1, sign_new) + S_psi_free(adj_new, self.psi, c2, sign_new)
@@ -100,6 +121,7 @@ class Manifold:
             if p > np.random.rand():
                 self.adj = adj_new
                 self.A = A_new
+                self.sign = sign_new
 
     def update_field(self, i, field, field_range, action, beta, is_complex=False, add_field=None):
         field_new = np.copy(field)

manifold_sampler/tests/spinor_test.py
@@ -1,7 +1,6 @@
 import sys
 import unittest
 
-import numpy as np
 import scipy
 
 sys.path.append("../")
@@ -40,6 +39,22 @@ class TestPsi(unittest.TestCase):
             product = np.matmul(psi_bar, psi)
             self.assertAlmostEqual(np.imag(product), 0)
 
-    ### test plaquette sign
+    def test_plaquette(self):
+        N = 16
+        m = Manifold(N)
+
+        # check if m.sign works in initial config
+        for i in range(len(m.adj)):
+            trace_plaquette, def_angle = circle_vertex(m.adj, m.sign, i)
+            self.assertAlmostEqual(trace_plaquette, np.cos(def_angle/2))
+
+        # m.sign works after evolution
+        m.sweep(10, 1, ['gravity', 'spinor_free'])
+        for i in range(len(m.adj)):
+            trace_plaquette, def_angle = circle_vertex(m.adj, m.sign, i)
+            print(np.cos(def_angle / 2))
+            self.assertAlmostEqual(trace_plaquette, np.cos(def_angle/2))
+
+
 if __name__ == '__main__':
     unittest.main()