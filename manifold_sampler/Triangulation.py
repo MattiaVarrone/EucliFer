update of signs for gauge links to be fixed

manifold_sampler/Triangulation.py
@@ -4,19 +4,19 @@ from Graph_utils import *
 rng = np.random.default_rng()
 
 
+# counter-clockwise circling of vertices to compute plaquettes and angle deficiency
 def circle_vertex(adj, sign, i):
-    j = next_(adj[i])
+    j = prev_(adj[i])
     alpha = theta[i % 3] - theta[adj[i] % 3] + np.pi
     U = sign[i] * paral_trans(alpha / 2)
     def_triangles = 6 - 1
 
-    # clockwise circling of vertices
     while j != i:
         alpha = theta[j % 3] - theta[adj[j] % 3] + np.pi
         U_1 = sign[j] * paral_trans(alpha / 2)
         U = np.matmul(U, U_1)
         def_triangles -= 1
-        j = next_(adj[j])
+        j = prev_(adj[j])
 
     return U, def_triangles
 
@@ -102,7 +102,7 @@ class Manifold:
                 sign_new[k] = sign_new[m]
                 sign_new[j] = sign_new[l]
 
-                for edge in (n, m, l):
+                for edge in (i, k, j):
                     U, def_triangles = circle_vertex(adj_new, sign_new, edge)
                     trace = np.trace(U)/2
                     trace_th = np.cos(def_triangles * np.pi / 6)

manifold_sampler/tests/spinor_test.py
@@ -43,14 +43,14 @@ class TestPsi(unittest.TestCase):
         N = 16
         m = Manifold(N)
 
-        # check if m.sign works in initial config
+        # check if the fan triangulation has a consistent sign configuration
         for i in range(len(m.adj)):
             U, def_triangles = circle_vertex(m.adj, m.sign, i)
             trace_plaquette = np.trace(U) / 2
             self.assertAlmostEqual(trace_plaquette, np.cos(def_triangles*np.pi/6))
 
-        # m.sign works after evolution
-        m.random_update(1, ['gravity', 'spinor_free'])
+        # check if edge flips preserve the consistency of the sign configuration
+        m.sweep(n_sweeps=1, beta=0.6, strategy=['gravity', 'spinor_free'])
         for i in range(len(m.adj)):
             U, def_triangles = circle_vertex(m.adj, m.sign, i)
             trace_plaquette = np.trace(U)/2