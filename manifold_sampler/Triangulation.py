fixed some bugs fro spinor parall transport. sign flags not yet antisymmetrised

manifold_sampler/Triangulation.py
@@ -6,14 +6,14 @@ rng = np.random.default_rng()
 
 # counter-clockwise circling of vertices to compute plaquettes and angle deficiency
 def circle_vertex(adj, sign, signc, i):
-    j = prev_(adj[i])
     alpha = theta[i % 3] - theta[adj[i] % 3] + np.pi
-    U = signc[i % 3] * sign[i] * paral_trans(alpha / 2)
+    U = signc[i // 3] * sign[i] * paral_trans(alpha / 2)
     def_triangles = 6 - 1
 
+    j = prev_(adj[i])
     while j != i:
         alpha = theta[j % 3] - theta[adj[j] % 3] + np.pi
-        U_1 = signc[j % 3] * sign[j] * paral_trans(alpha / 2)
+        U_1 = signc[j // 3] * sign[j] * paral_trans(alpha / 2)
         U = np.matmul(U, U_1)
         def_triangles -= 1
         j = prev_(adj[j])
@@ -131,6 +131,9 @@ class Manifold:
                     trace_th = np.cos(def_triangles * np.pi / 6)
                     sign_flip.append(np.sign(trace / trace_th))
 
+                sign_ = np.copy(sign_new)
+                signc_ = np.copy(signc_new)
+                traces = {}
                 ###### THIS IS THE IMPORTANT PART ######
                 # ss[x] is 1 if the plaquette corresp to edge x has correct sign, while it is -1 otherwise
                 ss = {}
@@ -140,22 +143,23 @@ class Manifold:
                     trace_th = np.cos(def_triangles * np.pi / 6)
 
                     if np.isclose(trace, 0):  ### problems when trace = 0
-                        s = np.sign(U[0, 1] / np.sin(def_triangles * np.pi / 6))
+                        s = np.sign(U[0, 1] / np.sin(def_triangles * np.pi / 6)) ### maybe should include minus sign
                     else:
                         s = np.sign(trace / trace_th)
                     ss[edge] = s
+                    traces[edge] = trace
 
                 # we enforce consistency relations ### check!
                 if j != n:
                     sign_new[k] *= ss[k]
                     sign_new[j] *= ss[i] * ss[j]
                     sign_new[i] *= ss[j] * ss[k] * ss[l]
-                    signc_new[i % 3] *= ss[i] * ss[j] * ss[k] * ss[l]
+                    signc_new[c1] *= ss[i] * ss[j] * ss[k] * ss[l]
 
-                    ### modify for case j == n
                     sign_new[m] = sign_new[k]
                     sign_new[l] = sign_new[j]
                     sign_new[n] = sign_new[i]
+
                 # the case j == n requires a distinct approach
                 else:
                     print("j == n")
@@ -187,18 +191,25 @@ class Manifold:
                         print(j==n)
                         t = 1
                 ss1 = {}
+                report = False
+                any_trace_zero = False
                 for edge in (i, j, k, l):
                     U, def_triangles = circle_vertex(adj_new, sign_new, signc_new, edge)
                     trace = np.trace(U) / 2
                     trace_th = np.cos(def_triangles * np.pi / 6)
 
                     if np.isclose(trace, 0):  ### problems when trace = 0
-                        s = 0.000001
+                        s = 0
+                        any_trace_zero = True
                     else:
                         s = np.sign(trace / trace_th)
                     ss1[edge] = s
                     if s != 1:
-                        print("s != 1")
+                        report = True
+                if report:
+                    print(ss1)
+                    print(f'{any_trace_zero = }')
+
 
                 # action variation
                 signs = (self.sign, self.signc)

manifold_sampler/main_hpc.py
@@ -3,7 +3,7 @@ from Analysis_utils import *
 from scipy.interpolate import CubicSpline
 
 # params for the lattice-matter system and sampling
-sizes = [(int(i) // 2) * 2 for i in np.geomspace(100, 500, 8)]
+sizes = [(int(i) // 2) * 2 for i in np.geomspace(30, 100, 8)]
 beta = 0.63
 matter = 'ising'
 strategy = ['gravity', matter]
@@ -19,67 +19,69 @@ if array_id is not None:
 def profile_maker(size):
     return make_profile(size, beta, strategy, eq_sweeps, meas_sweeps, n_measurements)
 
-if array_id is not None and array_id <= 1:
-    prof = make_profiles(sizes, beta, strategy, eq_sweeps, meas_sweeps, n_measurements)
-else:
-    prof = make_profiles_mp(sizes, profile_maker)
 
-# create plots
-fig, ax = plt.subplots(1, 2)
+if __name__ == '__main__':
 
-# fit a cubic spline through the distance profiles
-fit_prof = []
-prof_0 = prof[..., 0]  # neglect errors
-rs = np.arange(0.5, 10, 0.05)  # range for spline fit
+    # the process of creating and updating different lattices is parallelised.
+    pool = mp.Pool(mp.cpu_count())
+    with timebudget("Time to make profiles:"):
+        prof = np.array(pool.map(profile_maker, sizes))
 
-for profile in prof_0:
-    dist_range = np.arange(len(profile))
-    cs = CubicSpline(dist_range, profile)
-    fit_prof.append(cs(rs))
+    # create plots
+    fig, ax = plt.subplots(1, 2)
 
-    # plot distance profiles
-    ax[0].plot(dist_range, profile, 'x')
-    ax[0].plot(rs, cs(rs))
+    # fit a cubic spline through the distance profiles
+    fit_prof = []
+    prof_0 = prof[..., 0]  # neglect errors
+    rs = np.arange(0.5, 10, 0.05)  # range for spline fit
 
-np.savez('data/profiles_spinor_1', full_prof=prof, fit_prof=np.array(fit_prof), prof_0=prof_0, sizes=sizes, beta=beta)
+    for profile in prof_0:
+        dist_range = np.arange(len(profile))
+        cs = CubicSpline(dist_range, profile)
+        fit_prof.append(cs(rs))
 
-# make fit of finite size scaling in log-log space
-y = np.log(np.max(fit_prof, axis=1))
-x = np.log(sizes)
-fit = curve_fit(lin_fit, x, y, p0=[0.75, 0])
-(d, a), err = fit
-d_H = 1 / (1 - d)
-d_err = np.sqrt(err[0, 0])
-d_H_err = d_H ** 2 * d_err
-a_err = np.sqrt(err[1, 1])
+        # plot distance profiles
+        ax[0].plot(dist_range, profile, 'x')
+        ax[0].plot(rs, cs(rs))
 
-# make fit without spline
-d_H_ws, d_H_err_ws, a_ws, a_err_ws = finite_size_scaling(prof, sizes)
+    np.savez('data/profiles_spinor_1', full_prof=prof, fit_prof=np.array(fit_prof), prof_0=prof_0, sizes=sizes,
+             beta=beta)
 
+    # make fit of finite size scaling in log-log space
+    y = np.log(np.max(fit_prof, axis=1))
+    x = np.log(sizes)
+    fit = curve_fit(lin_fit, x, y, p0=[0.75, 0])
+    (d, a), err = fit
+    d_H = 1 / (1 - d)
+    d_err = np.sqrt(err[0, 0])
+    d_H_err = d_H ** 2 * d_err
+    a_err = np.sqrt(err[1, 1])
 
-##### plot settings #####
-ax[1].plot(x, lin_fit(x, d, a), 'g')
-ax[1].plot(x, lin_fit(x, 1 - 1 / d_H_ws, a_ws))
-ax[1].plot(x, y, 'gx')
+    # make fit without spline
+    d_H_ws, d_H_err_ws, a_ws, a_err_ws = finite_size_scaling(prof, sizes)
 
-ax[0].set_title('Distance Profiles'), ax[1].set_title('$Log(r_{max})\; vs\; Log(N)$')
-ax[0].legend(["N = " + str(N) for N in sizes]), ax[1].legend(["Spline", "No spline"])
-fig.suptitle(f'{matter = }, {beta = }')
+    ##### plot settings #####
+    ax[1].plot(x, lin_fit(x, d, a), 'g')
+    ax[1].plot(x, lin_fit(x, 1 - 1 / d_H_ws, a_ws))
+    ax[1].plot(x, y, 'gx')
 
-ID = "" if array_id is None else str(array_id)
-fig.set_size_inches(8, 5)
-plt.savefig("data/pics/dist_" + matter + "_" + ID + ".png")
-plt.show()
+    ax[0].set_title('Distance Profiles'), ax[1].set_title('$Log(r_{max})\; vs\; Log(N)$')
+    ax[0].legend(["N = " + str(N) for N in sizes]), ax[1].legend(["Spline", "No spline"])
+    fig.suptitle(f'{matter = }, {beta = }')
 
+    ID = "" if array_id is None else str(array_id)
+    fig.set_size_inches(8, 5)
+    plt.savefig("data/pics/dist_" + matter + "_" + ID + ".png")
+    plt.show()
 
-##### print results and info #####
-print('Number of cores: ' + str(mp.cpu_count()))
-print(f'{array_id = }')
+    ##### print results and info #####
+    print('Number of cores: ' + str(mp.cpu_count()))
+    print(f'{array_id = }')
 
-print("\nwith spline fit:")
-print(f'{d_H = }' + '+/-' + f'{d_H_err = }')
-print(f'{a = }' + '+/-' + f'{a_err = }')
+    print("\nwith spline fit:")
+    print(f'{d_H = }' + '+/-' + f'{d_H_err = }')
+    print(f'{a = }' + '+/-' + f'{a_err = }')
 
-print("\nwithout spline fit:")
-print(f'{d_H_ws = }' + '+/-' + f'{d_H_err_ws = }')
-print(f'{a_ws = }' + '+/-' + f'{a_err_ws = }')
+    print("\nwithout spline fit:")
+    print(f'{d_H_ws = }' + '+/-' + f'{d_H_err_ws = }')
+    print(f'{a_ws = }' + '+/-' + f'{a_err_ws = }')

manifold_sampler/tests/spinor_test.py
@@ -1,7 +1,7 @@
 import sys
 import unittest
 
-import scipy
+import scipy.linalg
 
 sys.path.append("../")
 from manifold_sampler.Triangulation import *
@@ -41,39 +41,40 @@ class TestPsi(unittest.TestCase):
             self.assertAlmostEqual(np.imag(product), 0)
 
     def test_plaquette(self):
-        N = 8
+        N = 20
         m = Manifold(N)
         strategy = ['gravity', 'spinor_free']
         # check if the fan triangulation has a consistent sign configuration
         for i in range(len(m.adj)):
             U, def_triangles = circle_vertex(m.adj, m.sign, m.signc, i)
             trace_plaquette = np.trace(U) / 2
-            self.assertAlmostEqual(trace_plaquette, np.cos(def_triangles*np.pi/6))
+            self.assertAlmostEqual(trace_plaquette, np.cos(def_triangles * np.pi / 6))
 
         # check if edge flips preserve the consistency of the sign configuration
-        for it in range(10*N):
+        for it in range(10 * N):
             m.random_update(beta=0.6, strategy=strategy)
             for i in range(len(m.adj)):
                 U, def_triangles = circle_vertex(m.adj, m.sign, m.signc, i)
-                trace_plaquette = np.trace(U)/2
-                #print(np.cos(def_triangles*np.pi/6))
-                self.assertAlmostEqual(trace_plaquette, np.cos(def_triangles*np.pi/6))
+                trace_plaquette = np.trace(U) / 2
+                self.assertAlmostEqual(trace_plaquette, np.cos(def_triangles * np.pi / 6))
             print(it)
+
     def test_gauge_sign(self):
         N = 16
         m = Manifold(N)
         strategy = ['gravity', 'spinor_free']
 
-        np.testing.assert_allclose(m.sign**2, np.ones(3*N))
+        np.testing.assert_allclose(m.sign ** 2, np.ones(3 * N))
         for edge in m.adj:
             self.assertEqual(m.sign[edge], m.sign[m.adj[edge]])
 
-        for _ in range(30*N):
+        for _ in range(30 * N):
             m.random_update(0.5, strategy)
             for edge in m.adj:
                 self.assertEqual(m.sign[edge], m.sign[m.adj[edge]])
-            np.testing.assert_allclose(m.sign**2, np.ones(3*N))
-            np.testing.assert_allclose(m.signc**2, np.ones(N))
+            np.testing.assert_allclose(m.sign ** 2, np.ones(3 * N))
+            np.testing.assert_allclose(m.signc ** 2, np.ones(N))
+
 
 if __name__ == '__main__':
     unittest.main()