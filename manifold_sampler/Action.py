need to figure out why D is not skew-symm and det(D) has wrong sign. Computation of Dirac operator is prob wrong

manifold_sampler/Action.py
@@ -55,6 +55,7 @@ def S_spinor(D):
 
 def Dirac_operator(adj, sign, D=None, triangles=None, A=None):
     N = len(adj) // 3
+    D0 = np.copy(D)  #delete after fix
     if D is None:
         D = np.zeros(shape=(2 * N, 2 * N))
     if triangles is None:
@@ -73,12 +74,17 @@ def Dirac_operator(adj, sign, D=None, triangles=None, A=None):
 
             alpha = theta[k] - theta[adj[edge] % 3] + np.pi
             U = sign[edge] * paral_trans((alpha + A[edge]) / 2)  # factor of 1/2 accounts for spinor transport
-            H_0 = 1/2 * np.cos(theta[k]) * np.matmul(id + gamma1, U) - 1/2 * np.sin(theta[k]) * np.matmul(id + gamma2, U) - id
+            # Check if we should subtract the identity from H_0
+            H_0 = 1/2 * np.cos(theta[k]) * np.matmul(id + gamma1, U) - 1/2 * np.sin(theta[k]) * np.matmul(id + gamma2, U)
             H = -_K * np.matmul(eps, H_0)
 
             for a in range(2):
                 for b in range(2):
                     D[i+a, j+b] = H[a, b]
+
+    det = np.linalg.slogdet(D)
+    if det[0] == -1:
+        print("sign(det) = -1")
     return D
 
 ### check action calculation thoroughly     ### check why psi tends to diverge

manifold_sampler/Triangulation.py
@@ -105,8 +105,8 @@ class Manifold:
                     trace = np.trace(U) / 2
                     trace_th = np.cos(def_triangles * np.pi / 6)
 
-                    if np.isclose(trace, 0):  ### problems when trace = 0
-                        s = np.sign(U[0, 1] / np.sin(def_triangles * np.pi / 6))  ### maybe should include minus sign
+                    if np.isclose(trace, 0):  # determines sign when trace = 0
+                        s = np.sign(U[0, 1] / np.sin(def_triangles * np.pi / 6))
                     else:
                         s = np.sign(trace / trace_th)
 

manifold_sampler/main.py
@@ -12,19 +12,5 @@ l.sweep(n_sweeps, beta, ["gravity", "spinor_free"])
 
 D1 = Dirac_operator(l.adj, l.sign)
 
-"""
-@timebudget
-def D(n):
-    for _ in range(n):
-        Dirac_operator(l.adj, l.sign)
-    return Dirac_operator(l.adj, l.sign)
 
-@timebudget
-def det(n):
-    for _ in range(n):
-        np.linalg.slogdet(D1)[0]
-    return np.linalg.slogdet(D1)[0]
-
-print(np.linalg.slogdet(D(100)))
-print(det(100))
-"""
+print(np.linalg.slogdet(D1))

manifold_sampler/tests/spinor_test.py
@@ -16,7 +16,7 @@ class TestGauge(unittest.TestCase):
         N = 102
         n_sweeps = 10
         m = Manifold(N)
-        strategy = ['gravity', 'spinor_inter']
+        strategy = ['gravity', 'spinor_free']
         for _ in range(2):
             m.sweep(n_sweeps, beta, strategy)
             for i in range(len(m.adj)):
@@ -82,21 +82,36 @@ class TestConnection(unittest.TestCase):
 class TestAction(unittest.TestCase):
     @timebudget
     def test_dirac_determinant_sign(self):
-        N = 102
+        N = 42
         m = Manifold(N)
         strategy = ['gravity', 'spinor_free']
 
         # check if the fan triangulation has a consistent dirac determinant sign
-        D = Dirac_operator(m.adj, m.sign)
-        det_sign = np.linalg.slogdet(D)[0]
+        det_sign = np.linalg.slogdet(m.D)[0]
         self.assertEqual(det_sign, 1)
 
         # check if edge flips preserve the consistency of the dirac determinant sign
         for it in range(10 * N):
             m.random_update(beta=0.6, strategy=strategy)
-            D = Dirac_operator(m.adj, m.sign)
-            det_sign = np.linalg.slogdet(D)[0]
-            self.assertEqual(det_sign, 1)
+            det_sign = np.linalg.slogdet(m.D)[0]
+            self.assertEqual(1, det_sign)
+            print(it)
+
+    def test_dirac_operator_antisymm(self):
+        N = 42
+        m = Manifold(N)
+        strategy = ['gravity', 'spinor_free']
+
+        # check if the fan triangulation has a consistent dirac determinant sign
+        D_sym = (m.D + m.D.T)/2
+        np.testing.assert_allclose(D_sym, np.zeros(m.D.shape))
+
+        # check if edge flips preserve the consistency of the dirac determinant sign
+        for it in range(10 * N):
+            m.random_update(beta=0.6, strategy=strategy)
+            D_sym = (m.D + m.D.T) / 2
+            np.testing.assert_allclose(D_sym, np.zeros(m.D.shape))
+            print(it)
 
 
 if __name__ == '__main__':